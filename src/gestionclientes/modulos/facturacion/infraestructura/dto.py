0c3be37acc0c3bd70200ebd2993b621edf63dfbd
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2025-02-23 22:07:23 -0500

    feat: se adiciona la infraestructura del modulo de facturacion

---
+++ src/gestionclientes/modulos/facturacion/infraestructura/dto.py
@@ -0,0 +1,19 @@
+"""DTOs para la capa de infrastructura del dominio de vuelos
+
+En este archivo usted encontrará los DTOs (modelos anémicos) de
+la infraestructura del dominio de vuelos
+
+"""
+
+from gestionclientes.config.db import db
+
+Base = db.declarative_base()
+
+
+class Facturacion(db.Model):
+    __tablename__ = "facturacion"
+    id = db.Column(db.String, primary_key=True)
+    medio_pago = db.Column(db.String, nullable=False)
+    id_cliente = db.Column(db.String, nullable=False)
+    monto = db.Column(db.Integer, nullable=False)
+    
---
+++ src/gestionclientes/modulos/facturacion/infraestructura/mapeadores.py
@@ -0,0 +1,50 @@
+""" Mapeadores para la capa de infrastructura del dominio de vuelos
+
+En este archivo usted encontrará los diferentes mapeadores
+encargados de la transformación entre formatos de dominio y DTOs
+
+"""
+
+from gestionclientes.modulos.facturacion.dominio.entidades import Facturacion
+from gestionclientes.modulos.facturacion.dominio.objetos_valor import (
+    IdCliente, MedioPago)
+from gestionclientes.seedwork.dominio.repositorios import Mapeador
+
+from .dto import Facturacion as FacturacionDTO
+
+
+class MapeadorFacturacion(Mapeador):
+
+    def obtener_tipo(self) -> type:
+        return Facturacion.__class__
+
+    def entidad_a_dto(self, entidad: Facturacion) -> FacturacionDTO:
+        
+        facturacion_dto = FacturacionDTO()
+        facturacion_dto.id = str(entidad.id)
+        facturacion_dto.medio_pago = entidad.medioPago
+        facturacion_dto.id_cliente = entidad.idCliente
+        facturacion_dto.monto = entidad.monto
+
+        return facturacion_dto
+
+    def dto_a_entidad(self, dto: FacturacionDTO) -> any:
+        if type(dto) is FacturacionDTO:
+            print("infra only dto")
+            facturacion = Facturacion(
+                dto.id, 
+                medioPago=dto.medio_pago,
+                idCliente=dto.id_cliente,
+                monto=dto.monto
+            )
+            return facturacion
+        print("infra all dto", dto)
+        facturacion = []
+        for facturacionDTO in dto:
+            facturacion.append(Facturacion(
+                medioPago=facturacionDTO.medio_pago,
+                idCliente=facturacionDTO.id_cliente,
+                monto=facturacionDTO.monto
+            ))
+        print("infra all dto Fin")
+        return facturacion
