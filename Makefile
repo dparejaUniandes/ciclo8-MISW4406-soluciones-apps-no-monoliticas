747beb631a2e7fd8f6b17d40ea495600d2d51808
luis.mendoza <l.mendozah@uniandes.edu.co>
2025-02-24 00:10:08 -0500

    adding automatizacion

--- Makefile
+++ Makefile
@@ -9,12 +9,17 @@ venv:
 	$(PIP) install --upgrade pip --extra-index-url https://pypi.org/simple
 # Instalar dependencias
 install: venv
-	$(PIP) install -r requirements.txt --extra-index-url https://pypi.org/simple
+	$(PIP) install --no-cache-dir -r requirements.txt --extra-index-url https://pypi.org/simple
 # Ejecutar la API en local con recarga automática
 run:
+	export FLASK_APP=src/gestionclientes/api; \
 	. $(VENV_DIR)/bin/activate && flask --app src/gestionclientes/api run
 # Eliminar el entorno virtual
 clean:
 	rm -rf $(VENV_DIR) __pycache__
 # Regenerar el entorno virtual y reinstalar dependencias
 reset: clean venv install
+
+
+run-compose:
+	docker-compose up --build
--- src/gestionclientes/config/db.py
+++ src/gestionclientes/config/db.py
@@ -1,8 +1,29 @@
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+
+# db = None
+
+# def init_db(app: Flask):
+#     global db 
+#     db = SQLAlchemy(app)
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-db = None
+db = SQLAlchemy()
+
+DB_HOST_URL = os.environ.get("DB_HOST_URL", "127.0.0.1:5432")
+DB_NAME = os.environ.get("DB_NAME", "gestionclientes")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "29062013")
+
 
 def init_db(app: Flask):
-    global db 
-    db = SQLAlchemy(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL', f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST_URL}/{DB_NAME}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking for better performance
+    
+    db.init_app(app)
