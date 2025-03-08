d90ac50b00d08dbf46445b7eb9f21a1c88c8b10d
dparejaUniandes <d.pareja@uniandes.edu.co>
2025-03-08 08:40:15 -0500

    changed the subscription to see if is possible message always delivery to saga consumer

--- src/gestionclientes/modulos/sagas/infraestructura/consumidores.py
+++ src/gestionclientes/modulos/sagas/infraestructura/consumidores.py
@@ -23,7 +23,7 @@ def suscribirse_a_eventos():
     cliente = None
     try:
         cliente = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
-        consumidor = cliente.subscribe('eventos-pago', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='gestionclientes-sub-eventos', schema=AvroSchema(EventoPagoRealizado))
+        consumidor = cliente.subscribe('eventos-pago', consumer_type=_pulsar.ConsumerType.Shared,subscription_name='sagas-sub-eventos', schema=AvroSchema(EventoPagoRealizado))
 
         while True:
             mensaje = consumidor.receive()
