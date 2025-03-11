86d47869aba4d7f865f6680228ed7fdbfdca66c4
dparejaUniandes P <d.pareja@uniandes.edu.co>
2025-03-11 00:02:31 +0000

    descomentado sqlite y event type agregado a esquema en notificaciones

--- src/notificaciones/modulos/notificaciones/infraestructura/schema/v1/eventos.py
+++ src/notificaciones/modulos/notificaciones/infraestructura/schema/v1/eventos.py
@@ -4,6 +4,18 @@ from notificaciones.seedwork.infraestructura.schema.v1.eventos import \
     EventoIntegracion
 
 
+class PagoConfirmadoPayload(Record):
+    """ Payload para el evento de pago confirmado """
+    id_correlacion = String()
+    tipo = String()
+    valor = String()
+    medio = String()
+
+class EventoPagoConfirmado(EventoIntegracion):
+    """ Evento de pago confirmado """
+    data = PagoConfirmadoPayload()
+    event_type = String()
+
 class NotificacionCreadaPayload(Record):
     id_correlacion = String()
     id_cliente = String()
